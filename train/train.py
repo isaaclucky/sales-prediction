1a5d00dc3a6beeb30c1ee6c1e660aee981ae654a
isaaclucky <isaaclucky88@gmail.com>
2022-09-07 22:55:27 +0300

    fix: includes train script

---
+++ train/train.py
@@ -0,0 +1,56 @@
+from pprint import pprint
+import dvc.api
+
+# from sklearn.ensemble import RandomForestRegressor
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_squared_error, r2_score
+
+import mlflow
+import mlflow.sklearn
+import numpy as np
+import pandas as pd
+import os, sys
+
+import io 
+
+path_parent = os.path.dirname(os.getcwd())
+os.chdir(path_parent)
+sys.path.insert(0, path_parent+'/scripts')
+
+
+path="data/df_train.csv"
+repo='https://github.com/isaaclucky/sales-prediction'
+version="V1.0"
+
+data_url = dvc.api.read(
+    path=path,
+    repo=repo,
+    rev=version,
+)
+
+mlflow.set_experiment('sales_prediction')
+
+
+
+def main():
+    # prepare example dataset
+    data = pd.read_csv(io.StringIO(data_url), sep=",")
+    print(data.columns)
+    
+    #log data params
+    # mlflow.log_param('data_url', data_url)
+    mlflow.log_param('data_version', version)
+    mlflow.log_param('input_rows', data.shape[0])
+    mlflow.log_param('input_colums', data.shape[1])
+    
+    
+    # score = lr.score(X_test, y_test)
+    
+    # print("Score: %s" % score)
+    # mlflow.log_metric("score", score)
+    # mlflow.sklearn.log_model(lr, "model")
+    print("Model saved in run %s" % mlflow.active_run().info.run_uuid)
+
+
+if __name__ == "__main__":
+    main()
